18a1d59b8d87a2fbbd2105b891263760431fec35
Daniil Velichko <danvelduck@gmail.com>
2018-06-18 12:57:36 +0800

    Implement Display for VNode

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,5 @@
 use element::VElement;
+use std::fmt;
 use text::VText;
 use types::CowString;
 
@@ -14,5 +15,112 @@ impl VNode {
             VNode::Element(element) => element.get_key(),
             _ => None,
         }
+    }
+}
+
+impl fmt::Display for VNode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fn fmt_indent(indent_level: usize, node: &VNode, f: &mut fmt::Formatter) -> fmt::Result {
+            let indent_string = "    ";
+            match node {
+                VNode::Element(element) => {
+                    // Begin opening tag
+                    write!(
+                        f,
+                        "{}<{}",
+                        indent_string.repeat(indent_level),
+                        element.get_tag()
+                    )?;
+
+                    // Classes
+                    let mut classes: Vec<&CowString> = element.get_classes().iter().collect();
+                    classes.sort_by(|a, b| a.cmp(b));
+
+                    if classes.len() > 0 {
+                        write!(f, " class=\"")?;
+                        for (index, class) in classes.iter().enumerate() {
+                            if index > 0 {
+                                write!(f, " ")?;
+                            }
+                            write!(f, "{}", class)?;
+                        }
+                        write!(f, "\"")?;
+                    }
+
+                    // Attributes
+                    let mut attr_pairs: Vec<(&CowString, &CowString)> =
+                        element.get_attributes().iter().collect();
+                    attr_pairs.sort_by(|(a, _), (b, _)| a.cmp(b));
+                    for (name, value) in attr_pairs {
+                        if value.len() > 0 {
+                            write!(f, " {}=\"{}\"", name, value)?;
+                        } else {
+                            write!(f, " {}", name)?;
+                        }
+                    }
+
+                    // End opening tag
+                    write!(f, ">")?;
+
+                    if element.get_children().len() > 0 {
+                        write!(f, "\n")?;
+                    }
+
+                    // Children
+                    for child in element.get_children() {
+                        fmt_indent(indent_level + 1, child, f)?;
+                    }
+
+                    // Closing tag
+                    write!(
+                        f,
+                        "{}</{}>\n",
+                        indent_string.repeat(indent_level),
+                        element.get_tag()
+                    )
+                }
+                VNode::Text(text) => write!(
+                    f,
+                    "{}{}\n",
+                    indent_string.repeat(indent_level),
+                    text.get_content()
+                ),
+            }
+        }
+
+        fmt_indent(0, self, f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tags::*;
+
+    #[test]
+    fn node_to_string() {
+        #[cfg_attr(rustfmt, rustfmt_skip)]
+        let node = div().class_list("aaa bbb").attr("id", "ccc").attr("hidden", "")
+            .child(p().class("one").text("1"))
+            .child(p().class("two").text("2"))
+            .child(p().class("three").text("3"))
+            .done();
+
+        let result = format!("\n{}", node.to_string());
+
+        let expected = r#"
+<div class="aaa bbb" hidden id="ccc">
+    <p class="one">
+        1
+    </p>
+    <p class="two">
+        2
+    </p>
+    <p class="three">
+        3
+    </p>
+</div>
+"#;
+
+        assert_eq!(result, expected);
     }
 }
